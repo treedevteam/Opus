4113d38f6a33f659eb6c13186750cc9019072c49
bleron.restelica@gmail.com <bleron.restelica@gmail.com>
2022-02-23 16:42:16 +0100

    test

--- src/app/modules/admin/tasks/tasks.service.ts
+++ src/app/modules/admin/tasks/tasks.service.ts
@@ -196,6 +196,21 @@ export class TasksService
      *
      * @param tag
      */
+
+     storeTask(form: any): Observable<Task2>{
+        return this._httpClient.post<Task2>('https://opus.devtaktika.com/api/task/store', form).pipe(
+            // eslint-disable-next-line arrow-body-style
+            map((data: Task2) => {
+                // this.task$.next(data);
+                return data;
+            }),
+            catchError((err) => {
+                console.error(err);
+                throw err;
+            }
+        )
+        );
+    }
     createTag(tag: Tag): Observable<Tag>
     {
         return this.tags$.pipe(
