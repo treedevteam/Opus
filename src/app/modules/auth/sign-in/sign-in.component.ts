e4a74d4da320283576ccd739012d0c021be8cf38
bleroniii <bleron.restelica@gmail.com>
2022-06-23 13:58:55 +0200

    test

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -46,20 +46,12 @@ export class AuthSignInComponent implements OnInit
     {
         // Create the form
         this.signInForm = this._formBuilder.group({
-<<<<<<< HEAD
             username     : ['lberge@example.net', [Validators.required, Validators.email]],
-=======
-            username     : ['rijad.morina@treedev.io', [Validators.required, Validators.email]],
->>>>>>> ac799c5a020937f6aa4ac35bdc6e5e0fae4b71c2
             password     : ['password', [Validators.required, Validators.minLength(8)]],
             grant_type   : ['password'],
             client_id    : ['1'],
             scope        : [''],
-<<<<<<< HEAD
             client_secret: ['OqRKUKhU8KK7k71u8TBJWJQUAKmTzgsK9yKvA1w3']
-=======
-            client_secret: ['A7lt43qJY0xGQBY7d7xPLWy5GRxmMU8xpukzRYm7']
->>>>>>> ac799c5a020937f6aa4ac35bdc6e5e0fae4b71c2
         });
     }
 
