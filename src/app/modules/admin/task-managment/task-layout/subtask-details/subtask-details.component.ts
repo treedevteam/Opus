6d138f3ecd0979416adc479cd7419700e5e92027
Lorik <lorik.vrellaku2005@gmail.com>
2023-07-21 14:37:04 +0200

    fixed subtask details

--- src/app/modules/admin/task-managment/task-layout/subtask-details/subtask-details.component.ts
+++ src/app/modules/admin/task-managment/task-layout/subtask-details/subtask-details.component.ts
@@ -1,5 +1,5 @@
 import {  Overlay, OverlayRef } from '@angular/cdk/overlay';
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, TemplateRef, ViewChild, ViewContainerRef, ChangeDetectorRef,HostListener,Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, TemplateRef, ViewChild, ViewContainerRef, ChangeDetectorRef, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { KanbanViewComponent } from '../../task-views/kanban-view/kanban-view.component';
@@ -31,8 +31,10 @@ export class SubtaskDetailsComponent implements OnInit, AfterViewInit, OnDestroy
     boardUsers$= this._taskService.boardUsers$;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
     taskOrSubtask = "";
-    subtasks:Task
+    subtask:any
+    subtaskDeadline: Date | null;
 
+    subtask$ = this._taskService.allSubTasks$;
     subtaskSelected$ = this._taskService.subtaskSelected$;
 
    
@@ -47,13 +49,21 @@ export class SubtaskDetailsComponent implements OnInit, AfterViewInit, OnDestroy
         private _changeDetectorRef: ChangeDetectorRef,
         private _overlay: Overlay,
         private _viewContainerRef: ViewContainerRef,
-
+        private _formBuilder:FormBuilder
 
     ) { }
 
-    ngOnInit(): void {
-       this._taskService.subtaskSelected$.subscribe(res=>{
-        this.subtasks = res
+    ngOnInit(): void {  
+      
+      this._taskService.subtaskSelected$.subscribe(res=>{
+        this.subtask = res
+        console.log(this.subtask, 'subtaskkk')
+      })
+
+       this._taskService.subtaskSelectedDetails$.subscribe(res=>{
+        this.subtask = res
+        this.subtaskDeadline = this.subtask.deadline;
+        console.log(this.subtask, 'subtaskkk')
       })
         
         //dont touch
